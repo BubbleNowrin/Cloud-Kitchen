5bdb31bfd6f456e036479fc3146c9e091d670329
NowrinIslamMim <nowrin02@gmail.com>
2022-11-09 23:37:22 +0600

    errorpage added

--- src/Components/Pages/ErrorPage/ErrorPage.js
+++ src/Components/Pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
+import Header from '../Shared/Header/Header';
+import errorPic from '../../../Assets/images/gif/9195-error.gif';
 
 const ErrorPage = () => {
+    const error = useRouteError();
+
     return (
         <div>
-            <h2>Error</h2>
+            <div>
+                <Header></Header>
+            </div>
+            <div className='mb-20 flex flex-col items-center justify-center'>
+                <img src={errorPic} alt="" />
+                <div>
+                    <h2 className='text-red-600 font-bold'>{error.status}</h2>
+                    <p className='text-red-600 text-2xl font-bold'>{error.statusText}</p>
+                </div>
+            </div>
+            <div>
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
