2fb5392ae1794ec0f327d37485130922ff3465d5
NowrinIslamMim <nowrin02@gmail.com>
2022-11-10 17:00:23 +0600

    foodcard changed

--- src/Components/Pages/Home/FoodCard.js
+++ src/Components/Pages/Home/FoodCard.js
@@ -8,46 +8,37 @@ const FoodCard = ({ food }) => {
     return (
 
         <div data-aos="zoom-in-down"
-            data-aos-duration="1000" className="group relative block bg-black">
+            data-aos-duration="1000" className='bg-gray-900 mb-6'>
 
-            <img
-                alt="Developer"
-                src={image}
-                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
-            />
-            <div className="relative p-8">
-                <p className="text-4xl font-bold text-lime-100 mb-4">{foodName}</p>
-                <p className="text-xl font-medium uppercase tracking-widest text-white">
+            <PhotoProvider>
+                <PhotoView src={image}>
+                    <img src={image} alt="" className='rounded-xl w-full object-cover h-40' />
+                </PhotoView>
+            </PhotoProvider>
+            <div class="relative text-center p-4">
+                <p className="text-2xl font-bold text-lime-100 mb-4">{foodName}</p>
+                <p className="text-lg font-medium uppercase tracking-widest text-lime-200 mb-3">
                     Price: ${price}
                 </p>
-                <div className="h-full w-full">
-                    <div
-                        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                <div>
+                    <p className="text-sm text-lime-100 mb-3">
+                        {details.length > 100 ? details.slice(0, 100) + "..." : details}
+                    </p>
+
+                    <Link
+                        className="group relative inline-block overflow-hidden border border-lime-600 px-8 py-3 focus:outline-none focus:ring"
+                        to={`/details/${_id}`}
                     >
-                        <PhotoProvider>
-                            <PhotoView src={image}>
-                                <img className='w-full h-32 object-cover' src={image} alt="" />
-                            </PhotoView>
-                        </PhotoProvider>
+                        <span
+                            className="absolute inset-x-0 bottom-0 h-[2px] bg-lime-600 transition-all group-hover:h-full group-active:bg-lime-500"
+                        ></span>
 
-                        <p className="text-sm text-white">
-                            {details.length > 100 ? details.slice(0, 100) + "..." : details}
-                        </p>
-                        <Link
-                            className="group relative inline-block overflow-hidden border border-lime-600 px-8 py-3 focus:outline-none focus:ring"
-                            to={`/details/${_id}`}
+                        <span
+                            className="relative text-sm font-medium text-lime-600 transition-colors group-hover:text-white"
                         >
-                            <span
-                                className="absolute inset-x-0 bottom-0 h-[2px] bg-lime-600 transition-all group-hover:h-full group-active:bg-lime-500"
-                            ></span>
-
-                            <span
-                                className="relative text-sm font-medium text-lime-600 transition-colors group-hover:text-white"
-                            >
-                                View Details
-                            </span>
-                        </Link>
-                    </div>
+                            View Details
+                        </span>
+                    </Link>
                 </div>
             </div>
         </div>
